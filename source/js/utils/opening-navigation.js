3d724212c4a2293c1f4904c66a847da90a46e829
Александр Паршин <cashkacaca@gmail.com>
2023-05-06 00:10:08 +0300

    Закрывается нажатие оверлей мобильное меню

--- source/js/utils/opening-navigation.js
+++ source/js/utils/opening-navigation.js
@@ -1,6 +1,7 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 let headerLogo = document.querySelector('.page-header__logo');
+let overlay = document.querySelector('.main-nav__overlay');
 
 const openingNavigation = () => {
   navMain.classList.remove('main-nav--nojs');
@@ -12,6 +13,14 @@ const openingNavigation = () => {
       navMain.classList.add('main-nav--opened');
       headerLogo.classList.add('page-header__logo--hidden');
     } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+      headerLogo.classList.remove('page-header__logo--hidden');
+    }
+  });
+
+  overlay.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--opened')) {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
       headerLogo.classList.remove('page-header__logo--hidden');
