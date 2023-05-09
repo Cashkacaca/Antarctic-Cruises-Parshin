ef89d3eed36c15e341d917841fcccc4e563b21d9
Александр Паршин <cashkacaca@gmail.com>
2023-05-09 23:36:59 +0300

    Убрал скролл за открытым мобильным меню

--- source/js/utils/opening-navigation.js
+++ source/js/utils/opening-navigation.js
@@ -2,6 +2,7 @@ let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
 let headerLogo = document.querySelector('.page-header__logo');
 let overlay = document.querySelector('.main-nav__overlay');
+let body = document.querySelector('body');
 
 const openingNavigation = () => {
   navMain.classList.remove('main-nav--nojs');
@@ -12,10 +13,12 @@ const openingNavigation = () => {
       navMain.classList.remove('main-nav--closed');
       navMain.classList.add('main-nav--opened');
       headerLogo.classList.add('page-header__logo--hidden');
+      body.classList.add('dis-scroll');
     } else {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
       headerLogo.classList.remove('page-header__logo--hidden');
+      body.classList.remove('dis-scroll');
     }
   });
 
@@ -24,6 +27,7 @@ const openingNavigation = () => {
       navMain.classList.add('main-nav--closed');
       navMain.classList.remove('main-nav--opened');
       headerLogo.classList.remove('page-header__logo--hidden');
+      body.classList.remove('dis-scroll');
     }
   });
 };
